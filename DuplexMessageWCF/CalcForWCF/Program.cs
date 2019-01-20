e23e767faaa43cd53af492fd0fc86da9ca161f07
Sanjay Gangwar <sanjay.gangwar143@gmail.com>
2019-01-20 16:56:07 +0530

    WCF

---
+++ DuplexMessageWCF/CalcForWCF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace CalcForWCF
+{
+    [CallbackBehavior (UseSynchronizationContext = false)]
+    public class Program : ServiceReference1.IService1Callback
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.abc();
+        }
+
+        public void abc() {
+            InstanceContext s = new InstanceContext(this);
+            ServiceReference1.Service1Client svc = new ServiceReference1.Service1Client(s);
+            svc.GetData(0);
+        }
+
+        void ServiceReference1.IService1Callback.Progress(int value)
+        {            
+            Console.WriteLine(value + "% process completed");
+        }
+    }
+}
